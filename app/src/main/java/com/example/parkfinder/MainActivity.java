bf5e3455bdaf43ff4f29c5eab0ec24741b8d5241
Arielwz <arieldududu@gmail.com>
2023-02-18 14:31:17 -0800

    Added non-functional AtYourService activity

--- app/src/main/java/com/example/parkfinder/MainActivity.java
+++ app/src/main/java/com/example/parkfinder/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.parkfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //dummy comment for testing github only, please ignore
+    }
+
+    public void AtYourServiceButton(View view) {
+        Intent intent = new Intent(this, AtYourService.class);
+        startActivity(intent);
     }
 }

61b0e1e02fcbf7c789dde0b342daa9e1e572f055
ellagooday <sabrinagooday@gmail.com>
2023-03-03 17:31:50 -0800

    Complete user sign in feature

--- app/src/main/java/com/example/parkfinder/StickItToEm.java
+++ app/src/main/java/com/example/parkfinder/StickItToEm.java
@@ -2,17 +2,41 @@ package com.example.parkfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StickItToEm extends AppCompatActivity {
+    private DatabaseReference dbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick_it_to_em);
+        dbReference = FirebaseDatabase.getInstance().getReference();
     }
 
+    // user sign in
     public void onClickSignIn(View view) {
+        EditText userInput = findViewById(R.id.editTextUserName);
+        String userName = userInput.getText().toString();
+        if (userName.isEmpty()) {
+            Toast.makeText(this, "Please enter a non-empty username.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        User user = new User(userName);
+        // add user to database, use the username as the key
+        dbReference.child("users").child(userName).setValue(user);
+        Toast.makeText(this, "Sign in successfully.", Toast.LENGTH_LONG).show();
+
+        Intent stickerIntent = new Intent(this, StickerSharing.class);
+        stickerIntent.putExtra("username", userName);
+        startActivity(stickerIntent);
     }
 }
---
+++ app/src/main/java/com/example/parkfinder/User.java
@@ -0,0 +1,13 @@
+package com.example.parkfinder;
+
+public class User {
+    private String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}

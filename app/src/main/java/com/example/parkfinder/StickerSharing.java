07b9d23cd99892c500e2e89e78eeb8f0f38b16eb
A3M4 <419714952@qq.com>
2023-03-04 05:21:37 -0500

    Add a feature of selecting a receiver and update the layout to a scroll view

--- app/src/main/java/com/example/parkfinder/StickerSharing.java
+++ app/src/main/java/com/example/parkfinder/StickerSharing.java
@@ -2,16 +2,27 @@ package com.example.parkfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.HashMap;
+import java.util.Set;
 
 public class StickerSharing extends AppCompatActivity {
     private String curUserName;
     private ImageView selectedStickerImageView;
+    private DatabaseReference databaseReference;
+    private final String FIREBASE_TAG = "FIREBASE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,9 @@ public class StickerSharing extends AppCompatActivity {
         // show welcome info
         TextView txtWelcomeInfo = findViewById(R.id.welcomeInfo);
         txtWelcomeInfo.setText(getString(R.string.welcome_info) + curUserName);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        initReceiverList();
 
     }
 
@@ -34,5 +48,27 @@ public class StickerSharing extends AppCompatActivity {
         selectedStickerImageView.setColorFilter(R.color.purple_200);
     }
 
+    // init spinner of user list for selecting a sender (excluding themselves)
+    private void initReceiverList() {
+        databaseReference.child("users").get().addOnCompleteListener(
+                task -> {
+                    if (!task.isSuccessful()) {
+                        Log.e(FIREBASE_TAG, "Error getting receiver list", task.getException());
+                    } else {
+                        HashMap usernameToUser = (HashMap) task.getResult().getValue();
+                        Set receiverSet = usernameToUser.keySet();
+                        receiverSet.remove(curUserName);
+                        Log.d(FIREBASE_TAG, receiverSet.toString());
+                        Spinner receiverSpinner = findViewById(R.id.spinner_select_receiver);
+                        receiverSpinner.setAdapter(new ArrayAdapter<>(this,
+                                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                                receiverSet.toArray()));
 
+                    }
+                });
+    }
+
+
+    public void sendSticker(View view) {
+    }
 }

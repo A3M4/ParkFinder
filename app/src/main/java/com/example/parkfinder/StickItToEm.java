e49708efd08ec13e448a3c95bf61082fec048250
ellagooday <sabrinagooday@gmail.com>
2023-03-03 16:57:57 -0800

    Add user sign in layout to Stick It To Em page

--- app/src/main/java/com/example/parkfinder/StickItToEm.java
+++ app/src/main/java/com/example/parkfinder/StickItToEm.java
@@ -3,6 +3,7 @@ package com.example.parkfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class StickItToEm extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class StickItToEm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick_it_to_em);
+    }
+
+    public void onClickSignIn(View view) {
     }
 }

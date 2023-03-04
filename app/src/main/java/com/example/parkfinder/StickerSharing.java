c53c24773951d2f933d45776c1e59c3ec311bb3d
A3M4 <419714952@qq.com>
2023-03-04 03:58:17 -0500

    Add user welcome info

--- app/src/main/java/com/example/parkfinder/StickerSharing.java
+++ app/src/main/java/com/example/parkfinder/StickerSharing.java
@@ -3,12 +3,19 @@ package com.example.parkfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StickerSharing extends AppCompatActivity {
+    private String curUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        curUserName = getIntent().getExtras().getString("username");
         setContentView(R.layout.activity_sticker_sharing);
+
+        // show welcome info
+        TextView txtWelcomeInfo = findViewById(R.id.welcomeInfo);
+        txtWelcomeInfo.setText(getString(R.string.welcome_info) + curUserName);
     }
 }

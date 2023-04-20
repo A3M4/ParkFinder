b877dca7556adf851b775b83d2cc0f9bdd6fe05f
eli03 <ethan_li@intuit.com>
2023-04-20 00:46:22 -0400

    revert commit delete current state function

--- app/src/main/java/com/example/parkfinder/nationalparks/activity/MapDisplayActivity.java
+++ app/src/main/java/com/example/parkfinder/nationalparks/activity/MapDisplayActivity.java
@@ -1,13 +1,7 @@
 package com.example.parkfinder.nationalparks.activity;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -16,7 +10,6 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.ActivityCompat;
@@ -40,11 +33,8 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 
 public class MapDisplayActivity extends AppCompatActivity implements
         OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
@@ -56,7 +46,7 @@ public class MapDisplayActivity extends AppCompatActivity implements
     private List<Park> parkList;
     private CardView cardView;
     private EditText stateCodeEt;
-    private String code;
+    private String code = "wa";
     private SupportMapFragment mapFragment;
 
     @Override
@@ -95,8 +85,6 @@ public class MapDisplayActivity extends AppCompatActivity implements
             }
             return false;
         });
-
-        setParksInUserStateInitialView();
 
         searchButton.setOnClickListener(view -> {
             HelperFunction.deactivateSoftInput(view);
@@ -153,33 +141,25 @@ public class MapDisplayActivity extends AppCompatActivity implements
         mMap.setOnInfoWindowClickListener(this);
 
         // Enable user location
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         } else {
             mMap.setMyLocationEnabled(true);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED
-                        || ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     mMap.setMyLocationEnabled(true);
                 }
+            } else {
+                // Permission denied, handle the case where the user denies the location permission
             }
         }
     }
@@ -215,113 +195,4 @@ public class MapDisplayActivity extends AppCompatActivity implements
         transaction.addToBackStack(null);
         transaction.commit();
     }
-
-    private void setParksInUserStateInitialView() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        } else {
-            // Get the user's current location
-            LocationManager locationManager =
-                    (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            Location myLocation =
-                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (myLocation == null) {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
-                        new LocationListener() {
-                            @Override
-                            public void onLocationChanged(Location location) {
-                                // Get the user's state code when location data becomes available
-                                String stateCode = getStateCode(location);
-                                locationManager.removeUpdates(this);
-                                code = getStateCode(stateCode);
-                            }
-                        });
-            } else {
-                // Get the user's state code from the location data
-                String stateCode = getStateCode(myLocation);
-                code = getStateCode(stateCode);
-            }
-        }
-    }
-
-    private String getStateCode(Location location) {
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-        try {
-            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
-            if (addresses.size() > 0) {
-                Address address = addresses.get(0);
-                return address.getAdminArea();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return "";
-    }
-
-    private static String getStateCode(String stateName) {
-        HashMap<String, String> stateCodeMap = new HashMap<>();
-        stateCodeMap.put("Alabama", "al");
-        stateCodeMap.put("Alaska", "ak");
-        stateCodeMap.put("Arizona", "az");
-        stateCodeMap.put("Arkansas", "ar");
-        stateCodeMap.put("California", "ca");
-        stateCodeMap.put("Colorado", "co");
-        stateCodeMap.put("Connecticut", "ct");
-        stateCodeMap.put("Delaware", "de");
-        stateCodeMap.put("Florida", "fl");
-        stateCodeMap.put("Georgia", "ga");
-        stateCodeMap.put("Hawaii", "hi");
-        stateCodeMap.put("Idaho", "id");
-        stateCodeMap.put("Illinois", "il");
-        stateCodeMap.put("Indiana", "in");
-        stateCodeMap.put("Iowa", "ia");
-        stateCodeMap.put("Kansas", "ks");
-        stateCodeMap.put("Kentucky", "ky");
-        stateCodeMap.put("Louisiana", "la");
-        stateCodeMap.put("Maine", "me");
-        stateCodeMap.put("Maryland", "md");
-        stateCodeMap.put("Massachusetts", "ma");
-        stateCodeMap.put("Michigan", "mi");
-        stateCodeMap.put("Minnesota", "mn");
-        stateCodeMap.put("Mississippi", "ms");
-        stateCodeMap.put("Missouri", "mo");
-        stateCodeMap.put("Montana", "mt");
-        stateCodeMap.put("Nebraska", "ne");
-        stateCodeMap.put("Nevada", "nv");
-        stateCodeMap.put("New Hampshire", "nh");
-        stateCodeMap.put("New Jersey", "nj");
-        stateCodeMap.put("New Mexico", "nm");
-        stateCodeMap.put("New York", "ny");
-        stateCodeMap.put("North Carolina", "nc");
-        stateCodeMap.put("North Dakota", "nd");
-        stateCodeMap.put("Ohio", "oh");
-        stateCodeMap.put("Oklahoma", "ok");
-        stateCodeMap.put("Oregon", "or");
-        stateCodeMap.put("Pennsylvania", "pa");
-        stateCodeMap.put("Rhode Island", "ri");
-        stateCodeMap.put("South Carolina", "sc");
-        stateCodeMap.put("South Dakota", "sd");
-        stateCodeMap.put("Tennessee", "tn");
-        stateCodeMap.put("Texas", "tx");
-        stateCodeMap.put("Utah", "ut");
-        stateCodeMap.put("Vermont", "vt");
-        stateCodeMap.put("Virginia", "va");
-        stateCodeMap.put("Washington", "wa");
-        stateCodeMap.put("West Virginia", "wv");
-        stateCodeMap.put("Wisconsin", "wi");
-        stateCodeMap.put("Wyoming", "wy");
-
-        return stateCodeMap.get(stateName);
-    }
 }
-
